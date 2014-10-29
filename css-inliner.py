e62add542a544ce6ea94ea333a99b87c059621cc
romainberger <romain@romainberger.com>
2014-10-29 16:59:41 +0100

    Initial commit :pizza:

---
+++ css-inliner.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+# Example:
+# ./css-inliner.py -f index.html -c main.css
+
+import urllib
+from lxml import html
+from converter import Conversion
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename", help="Input file")
+parser.add_option("-c", "--css",  dest="css",      help="Css file")
+
+def main():
+    (options, args) = parser.parse_args()
+    if options.filename is None or options.css is None:
+        raise Exception("Missing arguments filename and css")
+
+    htmlFile = open(options.filename)
+    htmlString = htmlFile.read()
+    cssFile = open(options.css)
+    css = cssFile.read()
+
+    document = html.fromstring(htmlString)
+
+    converter = Conversion()
+    converter.perform(document, document, css)
+
+    print converter.convertedHTML
+
+if __name__ == '__main__':
+    main()
